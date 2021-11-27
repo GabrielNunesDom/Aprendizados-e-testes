c3727eb1eeb760a8e294c8cf09a2f372d2ddfdbb
Gabriel Nunes Domingos <93409406+GabrielNunesDom@users.noreply.github.com>
2021-11-27 20:09:35 -0300

    rodoviaria.js
    Primeiros testes para um novo projeto. O intuito deste em questão é bem simples: começar a praticar JavaScript na prática.

---
+++ 1-rodoviaria.js
@@ -0,0 +1,41 @@
+console.log(`\n Testes para uma bilheteria de agência de viagens`)
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+const idadeComprador = 19;
+const estaAcompanhada = true;
+let temPassagemcomprada = false;
+const destino = "Salvador";
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = (idadeComprador >= 18 || estaAcompanhada == true);
+
+let contador = 0;
+let destinoExiste = false;
+while (contador < 3) {
+    if (listaDeDestinos[contador] == destino) {
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem!!!")
+}else{
+    console.log(" Desculpe tivemos um erro!");
+}
+
+for (let i = 0 ;i < 3; i ++) {
+    if (listaDeDestinos[contador] == destino) {
+        destinoExiste = true;
+        break;
+    }
+}
